e1ba83f07bf193ba92d24343584af55b55f0dd0a
Oluwabamise Olatunji <tunjibamise94@gmail.com>
2022-10-22 02:06:06 +0000

    Update index.js, create reset function, adjust loops and add Score variable

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-let username = prompt( "What is your name? ");
-console.log("Welcome " + username);
+var userName = prompt("What is your name? ");
+console.log(" Welcome " + userName);
 
- let score = 0;
- let level = 0;
+var score = 0;
+var level = 1;
+var range = 2;
+var whileCondition = true;
+var userinput = "";
 
-  for (let i = 2; i < 10; i++) {
-  let guess = Math.floor(Math.random() * i) + 1;
-  let userinput = parseInt(prompt("Enter a number between 1 and " + i));
-  console.log(userinput);
-    if (userinput == guess) {
-      level++;
-      console.log("You are in level " + level)
-      score++;
-      console.log("Your score is now " + score)
+function reset() {
+    console.log(`${userName}  try again pick a number between 1 and ${range} `);
+    userinput = prompt(`pick number betwin 1 and ${range} `);
+}
+
+while (whileCondition) {
+    if (userName) {
+        var randomNum = Math.floor(Math.random() * range) + 1;
+        var userinput = parseInt(prompt("Hi " + userName + " Enter a number between 1 and " + range + " "));
+        console.log(userinput);
+
+        var newNo = Math.floor(userinput);
+
+        if (randomNum === newNo) {
+            range++;
+            score++;
+          level++
+
+            console.log(`${userName} have gotten 1 point, you are now in level ${level} and your point is ${score}`);
+        } else {
+         reset();
+        }
+    } else {
+        console.log("enter your name");
+
+        whileCondition = false;
     }
-    let repeat = () => {
-        score = 0;
-        level = 0;
-        userinput = parseInt(prompt("Enter a number between 1 and " + i));
-    }
-    while (userinput != guess) {
-      repeat();
-    }
+}
