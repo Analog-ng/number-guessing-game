50a2ee5b577fb668995fa7347953f7d59b671bf9
Oluwabamise Olatunji <tunjibamise94@gmail.com>
2022-10-22 08:53:37 +0000

    updated range function

--- index.js
+++ index.js
@@ -1,29 +1,35 @@
-var userName = prompt("What is your name? ");
-console.log(" Welcome " + userName);
-
 var score = 0;
 var level = 1;
-var range = 2;
 var whileCondition = true;
 var userinput = "";
+let max = 2;
+let min = 1;
+var range = (max - min) + 1;
+
+function randomInRange(range){
+	return Math.floor(Math.random() * range) + 1;
+}
 
 function reset() {
     console.log(`${userName}  try again pick a number between 1 and ${range} `);
-    userinput = prompt(`pick number betwin 1 and ${range} `);
+    userinput = prompt(`pick number between 1 and ${range} `);
 }
+
+var userName = prompt("What is your name? ");
+console.log(" Welcome " + userName);
 
 while (whileCondition) {
     if (userName) {
-        var randomNum = Math.floor(Math.random() * range) + 1;
+        var randomNum = randomInRange(range);
         var userinput = parseInt(prompt("Hi " + userName + " Enter a number between 1 and " + range + " "));
-        console.log(userinput);
 
         var newNo = Math.floor(userinput);
 
         if (randomNum === newNo) {
+            max++
             range++;
             score++;
-          level++
+            level++
 
             console.log(`${userName} have gotten 1 point, you are now in level ${level} and your point is ${score}`);
         } else {
@@ -31,7 +37,6 @@ while (whileCondition) {
         }
     } else {
         console.log("enter your name");
-
         whileCondition = false;
     }
 }
